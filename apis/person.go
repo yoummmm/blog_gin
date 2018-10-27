b3807e8e6522752f1f9025ee4b746b22eeb47c9f
ixingjue <826739558@qq.com>
2018-10-27 09:46:54 +0800

    add test

--- apis/person.go
+++ apis/person.go
@@ -11,7 +11,16 @@ import (
 func IndexApi(c *gin.Context) {
 	c.String(http.StatusOK, "it is works")
 }
-
+func GetPersonsApi(c *gin.Context) {
+	p := Person{}
+	persons, err := p.GetPersons()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"persons": persons,
+	})
+}
 func AddPersonApi(c *gin.Context) {
 	firstName := c.Request.FormValue("first_name")
 	lastName := c.Request.FormValue("last_name")
--- router.go
+++ router.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	."blog_gin/apis"
-	)
+	. "blog_gin/apis"
+)
 
-
-func initRouter() *gin.Engine  {
-	router:=gin.Default()
-	router.GET("/",IndexApi)
+func initRouter() *gin.Engine {
+	router := gin.Default()
+	router.GET("/", IndexApi)
+	router.GET("/persons", GetPersonsApi)
 	router.POST("/person", AddPersonApi)
 	return router
 }
